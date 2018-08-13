d470b5329144748f7f1ad0e2bd9bcfebd3fd36f2
Krasimir Hristozov <krasi.hristozov@gmail.com>
2018-08-14 02:05:17 +0300

    Displaying the list of movies

--- src/Movies.js
+++ src/Movies.js
@@ -1,9 +1,75 @@
 import React, { Component } from 'react';
+import { Header, Message, Table } from 'semantic-ui-react';
+import { withAuth } from '@okta/okta-react';
 
-class Movies extends Component {
-    render() {
-        return <div>Movies page</div>
+import { API_BASE_URL } from './config'
+
+export default withAuth(class Movies extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: null,
+            isLoading: null
+        };
     }
-}
 
-export default Movies
+    componentDidMount() {
+        this.getMovies();
+    }
+
+    async getMovies() {
+        if (!this.state.movies) {
+            try {
+                this.setState({ isLoading: true });
+                const accessToken = await this.props.auth.getAccessToken();
+                const response = await fetch(API_BASE_URL + '/movies', {
+                    headers: {
+                        Authorization: `Bearer ${accessToken}`,
+                    },
+                });
+                const data = await response.json();
+                this.setState({ movies: data, isLoading: false});
+            } catch (err) {
+                this.setState({ isLoading: false });
+                console.error(err);
+            }
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <Header as="h1">My Movies</Header>
+                {this.state.isLoading && <Message info header="Loading movies..." />}
+                {this.state.movies &&
+                    <div>
+                        <Table>
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Title</th>
+                                    <th>Bad Puns Count</th>
+                                    <th>Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            {this.state.movies.map(
+                                    movie => 
+                                        <tr id={movie.id} key={movie.id}>
+                                            <td>{movie.id}</td>
+                                            <td>{movie.title}</td>
+                                            <td>{movie.count}</td>
+                                            <td>
+                                                Increase Count button
+                                            </td>
+                                        </tr>
+                            )}
+                            </tbody>
+                        </Table>
+                    </div>
+                }
+            </div>
+        );
+    }
+});
